38d752a3a92ee04893aac26656e355a439c15aff
Ivan Chowdhury <moaggaimc@gmail.com>
2024-05-13 19:50:34 -0400

    Create Unique_Paths.py

---
+++ Algorithms/2D Dynamic Programming/Unique_Paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        oldRow = [1] * (n)
+        for y in range(m-2, -1, -1):
+            newRow = [1] * (n)
+            for x in range(len(newRow)-2, -1, -1):
+                newRow[x] = newRow[x+1] + oldRow[x]
+            # print(oldRow, newRow)
+            oldRow = newRow
+                    
+        return oldRow[0]
