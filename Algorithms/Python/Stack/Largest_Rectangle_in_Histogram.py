22b2805d426aab0f0359bdad52181c1d92521c66
Ivan Chowdhury <moaggaimc@gmail.com>
2024-06-05 23:18:33 -0400

    Update Largest_Rectangle_in_Histogram.py

--- Algorithms/Python/Stack/Largest_Rectangle_in_Histogram.py
+++ Algorithms/Python/Stack/Largest_Rectangle_in_Histogram.py
@@ -1,34 +1,33 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         '''
-        loop through heights
-            compare val to top val of stack. if smaller, can't continue stack height anymore
-                pop from stack
-                getarea
-                    popped height * (cur index - popped index) 
-                    update leftmost index
-                update max area
-            place height, leftmost index on stack
-        loop through stack
-            pop val, index
-            getarea
-                popped height * len(array)
-            update max area
-        return maxarea
+        Use stack to reference previous columns
+        If current col >= top of stack, stack col can still be extended
+        If current col < top of stack: stack col can't be extended anymore. Pop, extend right to get area, and update res.
+            Pop shorter cols from stack
+            Calculate rectangle area (current i - popped i * min(h1, h2))
+            update largest rectangle
+            repeat until all shorter cols have been popped
+        Trick: When pushing to stack, push earliest popped i (current i if none were popped). This is to account for extending a column backwards to cover popped columns.
+        remaining cols in stack can be extended to end of array
+        
+        O(N) Time. 1 Pass with stack
+        O(N) Space. Stack can hold up to N columns at once.
         '''
 
         stack = []
         res = 0
-        for i, height in enumerate(heights):
-            leftmostI = i
-            while stack and stack[-1][1] > height:
+        for i, h in enumerate(heights):
+            tempI = i
+            while stack and stack[-1][1] > h:
                 poppedI, poppedH = stack.pop()
-                area = poppedH * (i - poppedI)
+                area = (i - poppedI) * poppedH
                 res = max(res, area)
-                leftmostI = poppedI
-            stack.append([leftmostI, height])
+                tempI = poppedI
+            stack.append((tempI, h))
         while stack:
             poppedI, poppedH = stack.pop()
-            area = poppedH * len(heights)
-            res = max(res, poppedH * (len(heights) - poppedI))
+            area = (len(heights) - poppedI) * poppedH
+
+            res = max(area, res)
         return res
